a6fddcca09c93eddd60da5236db97d3a205c4aca
Luis <otfgonzalez@gmail.com>
2022-06-05 20:43:13 -0700

    Three.js add to to node mod, working with three to add animations to home page. Loading spinner completed

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,12 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { Container, Figure, Fade , Row, Col, Table } from 'react-bootstrap';
+import { Container, Button, Fade , Row, Col, Table } from 'react-bootstrap';
 import axios from 'axios';
 import "../css/portfolio.css";
+import Spinner from "../Loader/Loader.js";
+import * as THREE from "three"
 
 export default function Portfolio() {
 
 
     const [loaded, setLoading] = useState(true);
+    const [loadingData, setLoadingData] = useState(false);
+
+    // Needed to Setup Three scene
+    const scene = new THREE.Scene();
+    // Params: Field of view, aspect ratio, near (plane), far (plane) 
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGL1Renderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+
+    // Adding a cube to scene
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: '#4287f5' });
+    const cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
+    camera.position.z = 5;
+
+
 
     const link = "https://api.github.com/users/lagonzalez1/repos";
     const [data, setData] = useState([]);
@@ -28,31 +49,48 @@ export default function Portfolio() {
         
 
     }, [])
+
+
+    const fetchData = () => {
+        setLoadingData(true);
+        loadObject()
+    }
+
+    const loadObject = () => {
+        requestAnimationFrame(loadObject);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+    }
+
     return ( 
         <>
+
+            <div className="container-fluid light gradient">
+                <h3 className="portfolio-title">Portfolio</h3>
+                <hr className="hr-sm" />
+            </div>
+
+
             <div className="container-fluid padding main">
-
-            
-
 
                 <Row>
                     <Col>
-                    
+                        h
                     </Col>
                 </Row>
 
             
                 <Row>
-                    <Col sm={4}>
+                    <Col sm={2}>
                     <div className="container-fluid head">
-                    <h3 className="font-loader">Portfolio</h3>
 
                     <Fade timeout={500} in={!loaded}>
                         <div id="example-fade-text">
                         
-                        <p className="font-loader">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-                        terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-                        labore wes anderson cred nesciunt sapiente ea proident.
+                        <p className="font-loader">
+                            Lists of project i have completed over the course of my computer science degree
+                            at California State University Los Angles.
                         </p>
                         </div>
                     </Fade>
@@ -61,39 +99,48 @@ export default function Portfolio() {
                     
                     </Col>
 
-                        <Col sm={8}>
+                        <Col sm={10}>
                         <div className="container-fluid padding">
-                            <Table striped hover variant="dark" >
+                            <Table striped hover variant="dark" size="sm">
                                 <thead>
                                     <tr>
-                                    <th>#</th>
-                                    <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>Username</th>
+                                        <th style={{width: "5%"}}>#</th>
+                                        <th style={{ width: "15%" }}>Name</th>
+                                        <th style={{ width: "20%" }}>Full Name</th>
+                                        <th>Url</th>
+                                        <th style={{ width: "7%" }}>View</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                    <td>1</td>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    </tr>
-                                    <tr>
-                                    <td>2</td>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                    </tr>
-                                    <tr>
-                                    <td>3</td>
-                                    <td colSpan={2}>Larry the Bird</td>
-                                    <td>@twitter</td>
-                                    </tr>
+                                    {loaded ? null : (data.map((item, index) => {
+                                        var count = index + 1;
+                                        return (
+                                            <tr key={count}>
+                                                <td> {count} </td>
+                                                <td> {item.name} </td>
+                                                <td> {item.full_name}</td>
+                                                <td> {item.url}</td>
+                                                <td><Button variant="primary" size="sm" onClick={fetchData} >View</Button></td>
+                                            </tr>
+                                            );
+
+                                    }))}
                                 </tbody>
                             </Table>
                         </div>                       
                         </Col>
+                </Row>
+
+
+                <Row>
+                    <Col>
+                        { loadingData ? <Spinner/> : <p className="font-loader"> Completed </p>}
+                    </Col>
+
+                    <Col>
+
+                    </Col>
+
                 </Row>
            
             </div>
